666af3486182fbea13f784b41d1914329310fd9c
Beginner-011 <87593658+Beginner-011@users.noreply.github.com>
2021-10-27 19:25:38 +0700

    Initial Commit

---
+++ javascript/portfolio.js
@@ -0,0 +1,6 @@
+const projectName = 'Personal Portfolio Webpage';
+const wherelearn = 'FCC FreeCodeCamp';
+const type = 'Project for Learning & Practice';
+const textEditor = 'Visual Studio Code (VSCode)';
+const whatUsed = 'HTML5 & CSS3';
+const developerNickname = 'Win';
