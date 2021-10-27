11b38fa0dfddcb839524903fc5e2b6d3c2811d9e
Beginner-011 <87593658+Beginner-011@users.noreply.github.com>
2021-10-27 20:19:21 +0700

    commit

--- index.js
+++
@@ -1,6 +0,0 @@
-const projectName = 'Personal Portfolio Webpage';
-const wherelearn = 'FCC FreeCodeCamp';
-const type = 'Project for Learning & Practice';
-const textEditor = 'Visual Studio Code (VSCode)';
-const whatUsed = 'HTML5 & CSS3';
-const developerNickname = 'Win';
